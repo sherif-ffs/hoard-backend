625aacb7d7eb87bc39730d1c056aa299e0947607
Sherif Elmetwally <sge5@njit.edu>
2022-01-28 21:46:28 -0500

    ts=>js

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importDefault = (this && this.__importDefault) || function(mod) {
+  return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 /* eslint-disable @typescript-eslint/no-var-requires */
@@ -21,34 +21,34 @@ const password = process.env.PASSWORD;
 const JWT_SECRET = process.env.JWT_SECRET;
 // Connect to MongoDB
 mongoose_1.default.connect(`mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const db = mongoose_1.default.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', function () {
-    console.log('Connected successfully');
+db.once('open', function() {
+  console.log('Connected successfully');
 });
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
+  origin: 'http://localhost:3000',
+  credentials: true,
 }));
 app.use((0, express_flash_1.default)());
 app.use((0, express_session_1.default)({
-    secret: JWT_SECRET,
-    resave: false,
-    saveUninitialized: false,
+  secret: JWT_SECRET,
+  resave: false,
+  saveUninitialized: false,
 }));
 // Passport Middleware
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 // Routes
 // Authentication routes
-app.use('/auth', require('./routes/auth.ts'));
+app.use('/auth', require('./routes/auth.js'));
 // Item routes
-app.use('/items', require('./routes/item.ts'));
+app.use('/items', require('./routes/item.js'));
 // Collection Routes
-app.use('/collections', require('./routes/collections.ts'));
+app.use('/collections', require('./routes/collections.js'));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server running'));
